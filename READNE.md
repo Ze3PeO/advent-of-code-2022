75fc562dcb17c3ea3476475b67b455f126336fa8
Philipp <philipp.lammers1@gmail.com>
2022-12-01 20:58:02 +0100

    Init project with the solution for day1

---
+++ READNE.md
@@ -0,0 +1,3 @@
+# advent-of-code-2022
+
+This is a simple PyCharm Python Project for Advent of Code 2022. I choose Python once again because Python is just that fcking good.
---
+++ puzzles/week1/day1.py
@@ -0,0 +1,19 @@
+def p1(inp):
+    return inp[-1]
+
+
+def p2(inp):
+    return sum(inp[-3:])
+
+
+with open('../../input/week1/day1.txt') as file:
+    line_tuples = file.read().split('\n\n')
+    calorie_tuples = [line_tuple.strip().split('\n') for line_tuple in line_tuples]
+    calorie_sums = []
+    for calorie_tuple in calorie_tuples:
+        calorie_sum = sum([int(calorie_count) for calorie_count in calorie_tuple])
+        calorie_sums.append(calorie_sum)
+    calorie_sums = sorted(calorie_sums)
+
+    print('part 1: {}'.format(p1(calorie_sums)))
+    print('part 2: {}'.format(p2(calorie_sums)))
