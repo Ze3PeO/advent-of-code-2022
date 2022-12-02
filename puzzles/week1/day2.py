6a86921333a603db7727c3fa46be76adc64ee668
Philipp <philipp.lammers1@gmail.com>
2022-12-02 19:00:51 +0100

    Add solution for day2

---
+++ puzzles/week1/day2.py
@@ -0,0 +1,42 @@
+def p1(inp):
+    # (what the opponent plays, what you should play): the score
+    p1_dict = {
+        ('A', 'X'): 4,
+        ('B', 'X'): 1,
+        ('C', 'X'): 7,
+        ('A', 'Y'): 8,
+        ('B', 'Y'): 5,
+        ('C', 'Y'): 2,
+        ('A', 'Z'): 3,
+        ('B', 'Z'): 9,
+        ('C', 'Z'): 6,
+    }
+
+    return sum([p1_dict[(opponent, result)] for opponent, result in inp])
+
+
+def p2(inp):
+    # (what the opponent plays, how the round ends): the score
+    p2_dict = {
+        ('A', 'X'): 3,
+        ('B', 'X'): 1,
+        ('C', 'X'): 2,
+        ('A', 'Y'): 4,
+        ('B', 'Y'): 5,
+        ('C', 'Y'): 6,
+        ('A', 'Z'): 8,
+        ('B', 'Z'): 9,
+        ('C', 'Z'): 7,
+    }
+
+    return sum([p2_dict[(opponent, result)] for opponent, result in inp])
+
+
+with open('../../input/week1/day2.txt') as file:
+    inp = []
+    for line in file:
+        (first, second) = line.strip().split(' ')
+        inp.append((first, second))
+
+    print('part 1: {}'.format(p1(inp)))
+    print('part 2: {}'.format(p2(inp)))
